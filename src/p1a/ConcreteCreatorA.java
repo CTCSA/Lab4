eb78ad7dfb9a08cd627906319c0b4753636aef14
ckc <mail@ckc.im>
2015-10-14 12:15:34 +0800

    initialize

---
+++ src/p1a/ConcreteCreatorA.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p1a;
+
+/**
+ *
+ * @author ckc
+ */
+public class ConcreteCreatorA extends Creator{
+    public factoryMethod(){
+        return ConcreteProductA();
+    }
+}
---
+++ src/p1a/Creator.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p1a;
+
+/**
+ *
+ * @author ckc
+ */
+public class Creator {
+    Product p = fatoryMethod();
+    System.out.println("Product Created: " + p.getName());
+}
